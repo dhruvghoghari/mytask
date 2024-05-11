49555d1d3dd4b530feee3f2073ea04885d1e77da
dhruvghoghari <128144781+dhruvghoghari@users.noreply.github.com>
2024-05-11 11:02:01 +0530

    3

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'app/routes/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
